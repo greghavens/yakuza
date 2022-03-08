5cf756c908f2267fe3800fae0d0287da50735306
halluz <greg@theinternetishard.com>
2022-03-08 16:58:46 -0600

    fixing menu.js for vercel build

--- src/pages/lore.js
+++ src/pages/lore.js
@@ -15,7 +15,7 @@ export default function Lore() {
         <link href="https://fonts.googleapis.com/css2?family=Road+Rage&display=swap" rel="stylesheet"/>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"/>
-        <script src="./menu.js" />
+        <script src="/menu.js" />
       </Helmet>
       <header>
         <section className="sub-header">
