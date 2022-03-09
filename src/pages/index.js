4c8528152acc2a7e0e1f4b04085a2ae82ecfb188
halluz <greg@theinternetishard.com>
2022-03-09 06:40:40 -0600

    Updating roadmap

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 import { Link } from "gatsby"
+import { AnchorLink } from "gatsby-plugin-anchor-links";
 
 import "../styles/style.css"
 
@@ -24,7 +25,8 @@ export default function Home() {
           <div className="nav-links" id="navLinks">
               <i className="fa fa-times" onclick="hideMenu()"></i>
               <ul>
-                  <li><Link to="/lore">LORE</Link></li>
+                  <li><AnchorLink to="#roadmap" stripHash>ROADMAP</AnchorLink></li>
+                  <li><Link href="/lore">LORE</Link></li>
                   <li><Link href="https://trade.dexlab.space/#/market/DpjaJ7YaH8oWcwiftg3Nn5k65jv3BiUitZjunfrgN9Cn">$BLOOD</Link></li>
                   <li><Link href="https://twitter.com/Yakuza_Raptorz"><i className="fab fa-twitter"></i></Link></li>
                   <li><Link href="https://t.co/lTH0MqHkbC"><i className="fab fa-discord"></i></Link></li>
@@ -48,7 +50,7 @@ export default function Home() {
       
 
         <section className="roadmap">
-          <h1>ROAD MAP</h1>
+          <h1><a name="roadmap">ROAD MAP</a></h1>
           <div className="column">
             <div className="row">
               <p><h2>PHASE 0</h2> <br />Mint 1111 unique Yakuza Raptorz and 1111 Yukan Theros <br />Secondary market Place Listing and Holder verification.</p>
@@ -57,20 +59,23 @@ export default function Home() {
               <p><h2>PHASE 1</h2> <br /> Ikari Raptor leaves in SHAME. <br />New, stronger, doxxed dev team initiates new roadmap </p> 
             </div>
             <div className="row">
-              <p><h2>PHASE 2</h2> <br />Token Generation - $YAK will be generated to replace $BLOOD with real <br /> tokenomics and intial amount will airdropped to Raptorz and Theros <br />holders, making them whole from their previous $BLOOD<br/> (claimed or unclaimed) </p> 
+              <p><h2>PHASE 2</h2> <br />Token Generation - $YAK will be generated to replace $BLOOD with real <br /> tokenomics and an intial amount will be airdropped to Raptorz and Theros <br />holders, making them whole from their previous $BLOOD<br/> (claimed or unclaimed) </p> 
             </div>
             <div className="row">
-              <p><h2>PHASE 3</h2><br />Use $YAK and SOL to evolve Yakuza Raptorz to their new form <br /> with updated, beautiful art  and royalties<br /> that feed the Yakuza Reborn community wallet.</p> 
+              <p><h2>PHASE 3</h2><br />Use $YAK and $SOL to evolve Yakuza Raptorz to their new form <br /> with updated, beautiful art  and royalties<br /> that feed the Yakuza Reborn community wallet.</p> 
             </div>
             <div className="row">
               <p><h2>PHASE 4</h2> <br />28 days from the start of evolution, an extinction event will occur. <br /> All non-evolved Raptorz will be left in the original, abandoned collection.<br /> 75% of the mint and 50% of royalties from secondary market <br />will be used in the creation of the custom Liquidity Pool <br /> for the $YAK/$SOL pair.</p> 
             </div>
             <div className="row">
-              <p><h2>PHASE 5</h2></p> 
+              <p><h2>PHASE 5</h2> <br /> Theros, lusting after the Raptorz and wanting a family, give their <br />$YAK and their life to make an egg. The last of Ikari's royalties are burned with the Theros</p> 
               
             </div>
             <div className="row">
-              <p><h2>PHASE 6</h2></p> 
+              <p><h2>PHASE 6</h2><br /> Incubation begins. For a fee payable in $YAK, Raptorz may incubate <br/> their eggs with "chance to hatch mechanics" for a small $YAK fee.</p> 
+            </div>
+            <div className="row">
+              <p><h2>PHASE 7</h2> When the first of the new breed is born, the next extinction even occurs. <br /> All remaining Theros will be abandoned or burned. The new <br/> breed produces more $YAK and requires $YAK for its developing world.</p>
             </div>
           </div>
         </section>
