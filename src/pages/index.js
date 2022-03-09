29df043f3914e5621e7a820029211d7518b28125
halluz <greg@theinternetishard.com>
2022-03-08 20:18:26 -0600

    updating roadmap start

--- src/pages/index.js
+++ src/pages/index.js
@@ -51,21 +51,26 @@ export default function Home() {
           <h1>ROAD MAP</h1>
           <div className="column">
             <div className="row">
-              <p>PHASE 1 <br />Mint 1111 unique Yakuza Raptorz.</p> 
+              <p><h2>PHASE 0</h2> <br />Mint 1111 unique Yakuza Raptorz and 1111 Yukan Theros <br />Secondary market Place Listing and Holder verification.</p>
             </div>
             <div className="row">
-              <p>PHASE 2 <br />Secondary market Place Listing and Holders SolaLandHQ verification. </p> 
+              <p><h2>PHASE 1</h2> <br /> Ikari Raptor leaves in SHAME. <br />New, stronger, doxxed dev team initiates new roadmap </p> 
             </div>
             <div className="row">
-              <p>PHASE 3 <br />Token Generation - $BLOOD generated right after the mint and <br /> airdropped to Raptor holders.</p> 
+              <p><h2>PHASE 2</h2> <br />Token Generation - $YAK will be generated to replace $BLOOD with real <br /> tokenomics and intial amount will airdropped to Raptorz and Theros <br />holders, making them whole from their previous $BLOOD<br/> (claimed or unclaimed) </p> 
             </div>
             <div className="row">
-              <p>PHASE 4 <br />Move the 50% of our sales royalties from secondary market places to <br /> the 
-              community wallet which will be used in Liquidity Pool <br /> for the $BLOOD/$USDC pair.</p> 
+              <p><h2>PHASE 3</h2><br />Use $YAK and SOL to evolve Yakuza Raptorz to their new form <br /> with updated, beautiful art  and royalties<br /> that feed the Yakuza Reborn community wallet.</p> 
             </div>
             <div className="row">
-              <p>PHASE 5 <br />Collaborations with bigger projects and individuals within 
-              the NFT space<br /> Perks-promotions,exclusive gifts for holders <br /> and whitelist Spots.</p> 
+              <p><h2>PHASE 4</h2> <br />28 days from the start of evolution, an extinction event will occur. <br /> All non-evolved Raptorz will be left in the original, abandoned collection.<br /> 75% of the mint and 50% of royalties from secondary market <br />will be used in the creation of the custom Liquidity Pool <br /> for the $YAK/$SOL pair.</p> 
+            </div>
+            <div className="row">
+              <p><h2>PHASE 5</h2></p> 
+              
+            </div>
+            <div className="row">
+              <p><h2>PHASE 6</h2></p> 
             </div>
           </div>
         </section>
