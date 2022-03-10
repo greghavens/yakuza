62b6f21dcaaf4714014cc257e5b891c6aae7992f
halluz <greg@theinternetishard.com>
2022-03-10 15:08:00 -0600

    fixing mobile menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,35 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import { AnchorLink } from "gatsby-plugin-anchor-links";
+import { useState } from "react"
+
+
 
 export default function Navbar() {
+  const [navbarOpen, setNavbarOpen] = useState(false)
+
+  const handleToggle = () => {
+    setNavbarOpen(prev => !prev)
+  }
+
+  const closeMenu = () => {
+    setNavbarOpen(false)
+  }
+
   return (
         <nav>
           <Link to="/"><img src="logo2.png" alt="logo"/></Link>
-          <div className="nav-links" id="navLinks">
-              <i className="fa fa-times" onclick="hideMenu()" onKeyDown="hideMenu()"></i>
+          <div className={`mobile-button ${navbarOpen ? "showMenu" : "fa-bars"}`} onClick={handleToggle}></div>
+          <div className={`nav-links ${navbarOpen ? "showMenu" : ""}`} id="navLinks">
+              
               <ul>
-                  <li><AnchorLink to="/#roadmap" >ROADMAP</AnchorLink></li>
-                  <li><AnchorLink to="/#lore">LORE</AnchorLink></li>
+                  <li><Link to="/#roadmap" onClick={() => closeMenu()}>ROADMAP</Link></li>
+                  <li><Link to="/#lore" onClick={() => closeMenu()}>LORE</Link></li>
                   <li><Link href="https://trade.dexlab.space/#/market/DpjaJ7YaH8oWcwiftg3Nn5k65jv3BiUitZjunfrgN9Cn">$BLOOD</Link></li>
                   <li><Link href="https://twitter.com/Yakuza_Raptorz"><i className="fab fa-twitter"></i></Link></li>
                   <li><Link href="https://t.co/lTH0MqHkbC"><i className="fab fa-discord"></i></Link></li>
               </ul>
           </div>
-          <i className="fa fa-bars" onclick="showMenu()" onKeydown="showMenu"></i>
+
         </nav>
   )
 }
