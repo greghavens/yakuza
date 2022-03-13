58a9ff4d2c6301faae26e43d397a38bdf7824b32
greghavens <greg@theinternetishard.com>
2022-03-13 08:02:28 -0500

    replacing fa icons with react-icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import { useState } from "react"
-
-
+import { FaBars } from "react-icons/fa"
+import { FaTwitter } from 'react-icons/fa'
+import { FaDiscord } from 'react-icons/fa'
 
 export default function Navbar() {
   const [navbarOpen, setNavbarOpen] = useState(false)
@@ -18,15 +19,15 @@ export default function Navbar() {
   return (
         <nav>
           <Link to="/"><img src="logo2.png" alt="logo"/></Link>
-          <div className={`mobile-button ${navbarOpen ? "showMenu" : "fa-bars"}`} onClick={handleToggle} onKeyDown={handleToggle}></div>
+          <div className={`mobile-button ${navbarOpen ? "showMenu" : ""}`} onClick={handleToggle} onKeyDown={handleToggle}><FaBars classname="nav-links" /></div>
           <div className={`nav-links ${navbarOpen ? "showMenu" : ""}`} id="navLinks">
               
               <ul>
                   <li><Link to="/#roadmap" onClick={() => closeMenu()}>ROADMAP</Link></li>
                   <li><Link to="/#lore" onClick={() => closeMenu()}>LORE</Link></li>
                   <li><Link href="https://trade.dexlab.space/#/market/DpjaJ7YaH8oWcwiftg3Nn5k65jv3BiUitZjunfrgN9Cn">$BLOOD</Link></li>
-                  <li><Link href="https://twitter.com/Yakuza_Raptorz"><i className="fab fa-twitter"></i></Link></li>
-                  <li><Link href="https://t.co/lTH0MqHkbC"><i className="fab fa-discord"></i></Link></li>
+                  <li><Link href="https://twitter.com/Yakuza_Raptorz"><FaTwitter/></Link></li>
+                  <li><Link href="https://t.co/lTH0MqHkbC"><FaDiscord/></Link></li>
               </ul>
           </div>
 
